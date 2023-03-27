8c9d4e9282414f419c53007325a3bfe7d3ea03e7
Liam Duffy <liamdanielduffy@gmail.com>
2023-03-27 01:41:37 -0400

    responds with html of city council calendar page 1

--- scraper.ts
+++ scraper.ts
@@ -1,3 +1,7 @@
 import { serve } from "https://deno.land/std@0.177.0/http/server.ts";
 
-serve((_) => new Response("Hello World"));
+serve(async (_) => {
+    const result = await fetch("https://legistar.council.nyc.gov/Calendar.aspx");
+    const html = await result.text();
+    return new Response(html);
+});
